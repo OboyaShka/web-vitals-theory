5c0deb8cdabe91451c12f3e33735b1956836c5fa
Poznanskiy Denis <dpoznanskiy@yandex.ru>
2023-05-12 03:53:05 +0300

    parser

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ onLCP(console.log);
 const url = 'https://habr.com/ru/companies/tinkoff/articles/page';
 (async function parse() {
     const result: any = [];
-    let promises = Array.from(Array(19).keys()).map(index => {
+    let promises = Array.from(Array(20).keys()).map(index => {
         return () => fetch(url + (index + 1) + '/', {
             headers: {
                 'Content-Type': 'text/html'
@@ -23,11 +23,14 @@ const url = 'https://habr.com/ru/companies/tinkoff/articles/page';
                 const articles = doc.getElementsByTagName('article');
 
                 for (let article of Array.from(articles)) {
-                    const isAngular = article.getElementsByClassName('tm-article-snippet__hubs-container')[0].textContent?.includes('Angular');
-                    if (isAngular) {
+                    const snippet = article.getElementsByClassName('tm-article-snippet__hubs-container')[0]?.textContent;
+                    if (snippet && (snippet.includes('Angular') || snippet.includes('JavaScript') || snippet.includes('TypeScript') || snippet.includes('HTML')) && !snippet.includes('ReactJS') && !snippet.includes('NestJS')) {
                         const title = article.getElementsByClassName('tm-title_h2')[0];
+                        //@ts-ignore
+                        const date = article.getElementsByClassName('tm-article-datetime-published')[0].firstChild?.title.split(' ')[0].slice(0, -1).split('-').reverse().join('.');
+                        const time = article.getElementsByClassName('tm-article-reading-time__label')[0];
                         const link = title.getElementsByTagName('a')[0];
-                        result.push(`[${title.textContent}](${link.href.replace('http://localhost:3000', 'https://habr.com')}) | :white_large_square:`);
+                        result.push(`[${title.textContent}](${link.href.replace('http://localhost:3000', 'https://habr.com')}) | ${date} | ${time.innerHTML.trim()} | :white_large_square:`);
                     }
                 }
             })
